9d9620a786d2b615222fffdc11d121c4f10b6641
Jolina Ragadi <jolinaragadi03@gmail.com>
2026-01-16 18:07:11 +0800

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,12 @@
+function sample() {
+    //old
+    var name = 'Jolina';
+    
+    //new
+    const num1 = 100;
+    let num2 = 200;
+    num2 = 500;
+
+    const sum = num1 + num2;
+    alert("The sum is" + sum);
+}
